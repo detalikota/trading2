7ed92b085c216bb97a53c61f3fe98551715dc292
detalikota <detalikota@gmail.com>
2020-11-30 23:47:12 +0300

    yahoogit add .!

--- TD2/main/views.py
+++ TD2/main/views.py
@@ -14,14 +14,22 @@ posts = [
         'title': 'Post #2',
         'content': 'My content of the post #2',
         'date_posted': 'December 1, 2020'
+    },
+    {
+        'author': 'Ruslan',
+        'title': 'Post #3',
+        'content': 'My content of the post #2',
+        'date_posted': 'December 1, 2020'
     }
 ]
+title = "wow"
 def home(request):
     
     return render(request, 'main/home.html')
 
 def about(request):
     context = {
-        'posts': posts
+        'cat': posts,
+        'title': title
     }
     return render(request, 'main/about.html', context)
