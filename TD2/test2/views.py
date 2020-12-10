9d3a322d4eb66053a4b87c9bc73a872cf18d27bc
detalikota <detalikota@gmail.com>
2020-12-11 01:18:21 +0300

    So many new stuff!

--- TD2/test2/views.py
+++ TD2/test2/views.py
@@ -1,29 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post2
 
-posts = [
-    {
-    'author': 'Magomed',
-    'date_posted': 'December',
-    'title': 'Post #1',
-    'content': 'Content of post #1'
-    },
-    {
-    'author': 'Ahmed',
-    'date_posted': 'November',
-    'title': 'Post #2',
-    'content': 'Content of post #2'
-    },
-]
-
-def func():
-    for x in range(50):
-        print (x)
-    return x
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post2.objects.all()
         }
     return render(request, "test2/home.html", context) # I can put the third argument here, called context to put some information into the templates
 
