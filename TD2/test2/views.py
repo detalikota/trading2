1aaafb6bf7d304be1896efb5ea0963fc40893b23
detalikota <detalikota@gmail.com>
2020-12-17 17:28:12 +0300

    yeah

--- TD2/test2/views.py
+++ TD2/test2/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post2
 
 
@@ -8,6 +9,47 @@ def home(request):
         'posts': Post2.objects.all()
         }
     return render(request, "test2/home.html", context) # I can put the third argument here, called context to put some information into the templates
+
+class PostListView(ListView):
+    model = Post2
+    template_name = 'test2/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+class PostDetailView(DetailView):
+    model = Post2
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post2
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post2
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post2
+    success_url = '/test'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
        
